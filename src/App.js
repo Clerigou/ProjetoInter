ae624553b9380bff4fa9a0cd059e1e08bfbdf60f
Carlos <carlosaalves07@outlook.com>
2022-10-18 14:33:25 -0300

    feat: Home screen changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import Navegacao from './Navigator';
+import CadastroPets from './screens/CadastroPets';
+import Home from './screens/Home';
 
 const App = () => {
-  return <Navegacao />;
+  return <Home />;
 };
 
 export default App;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,22 +1,95 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  StatusBar,
+  ImageBackground,
+  Image,
+} from 'react-native';
+import {colors} from '../commonStyles';
 
 const Home = ({navigation}) => {
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => navigation.navigate('CadastroPets')}>
-        <Text>Home</Text>
-      </TouchableOpacity>
-    </View>
+    <ImageBackground
+      source={require('../../assets/images/Segunda_tela_background.png')}
+      style={styles.container}>
+      <Text style={styles.textUser}>Oi, Gabriel</Text>
+      <Text style={styles.textIntro}>Selecione uma {'\n'}atividade</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.botao_text}>Cadastro Pets</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.botao_text}>Cadastros Usuários</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Image
+        source={require('../../assets/images/Terceira_tela_IMG.png/')}
+        style={styles.imageDogs}
+      />
+
+      <Image
+        source={require('../../assets/images/Terceira_tela_Elemento.png/')}
+        style={styles.elementBottomImage}
+      />
+    </ImageBackground>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 24,
+  },
+  textUser: {
+    color: colors.input,
+    fontSize: 28,
+  },
+  textIntro: {
+    color: colors.background_primary_dark,
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+
+  buttonContainer: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-  body: {},
+  botao: {
+    width: '70%',
+    height: 70,
+    borderRadius: 23,
+    backgroundColor: colors.input,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  botao_text: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: colors.text,
+  },
+  imageDogs: {
+    width: '100%',
+    height: '20%',
+    position: 'absolute',
+    backgroundColor: 'black',
+  },
+  elementBottomImage: {
+    width: '120%',
+    height: '20%',
+    position: 'absolute',
+    bottom: 0,
+  },
 });
+
+{
+  /* <TouchableOpacity onPress={() => navigation.navigate('CadastroPets')}>
+ 
+</TouchableOpacity> */
+}
 
 export default Home;
